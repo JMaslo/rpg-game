6c771fb9dabb355f80150110cd7b7daab838499f
J_Maslo <hry.v@post.cz>
2024-09-13 21:28:34 +0200

    Deleted RpgGame#playerMove(), fixed moving and wall collision

--- src/main/java/cz/jmaslo/RpgGame.java
+++ src/main/java/cz/jmaslo/RpgGame.java
@@ -8,17 +8,12 @@ import java.awt.*;
 
 public class RpgGame extends GameWindow {
 
-    private static final double SPEED = 750;
-    private static final int SIZE = 54;
-
-    double PlayerY = 100;
-    double PlayerX = 100;
+    private int playerY = 5;
+    private int playerX = 5;
 
     private static final int TILE_SIZE = 54;
     private static final int MAP_WIDTH = 11;
     private static final int MAP_HEIGHT = 11;
-
-
 
     private final int[][] map = {
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -40,8 +35,7 @@ public class RpgGame extends GameWindow {
 
     @Override
     public void render(Renderer r) {
-
-        for (int y = 0;y < MAP_HEIGHT; y++) {
+        for (int y = 0; y < MAP_HEIGHT; y++) {
             for (int x = 0; x < MAP_WIDTH; x++) {
                 if (map[y][x] == 0) {
                     r.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, Color.GREEN.getRGB());
@@ -50,77 +44,34 @@ public class RpgGame extends GameWindow {
                 }
             }
         }
-        r.fillRect((int) PlayerX, (int) PlayerY, SIZE, SIZE, Color.RED.getRGB());
+        r.fillRect(playerX * TILE_SIZE, playerY * TILE_SIZE, TILE_SIZE, TILE_SIZE, Color.RED.getRGB());
     }
 
     @Override
     public void update(double delta) {
 
-        double moveX = 0;
-        double moveY = 0;
+        int newX = playerX;
+        int newY = playerY;
 
         if (isKeyPressed(Key.W)) {
-            moveY -= TILE_SIZE;
+            newY--;
         }
         if (isKeyPressed(Key.S)) {
-            moveY += TILE_SIZE;
+            newY++;
         }
         if (isKeyPressed(Key.A)) {
-            moveX -= TILE_SIZE;
+            newX--;
         }
         if (isKeyPressed(Key.D)) {
-            moveX += TILE_SIZE;
+            newX++;
         }
 
-        double newPlayerX = PlayerX + moveX;
-        double newPlayerY = PlayerY + moveY;
-
-        PlayerX = newPlayerX;
-        PlayerY = newPlayerY;
-
-        playerMove();
-//        checkWallCollision();
-    }
-
-    public void checkWallCollision() {
-
-        // Check if there is a collision
-        // When player would like to go throw the wall, it will send him back to his last position
-
-        for (int i = 0; i < MAP_HEIGHT; i++) {
-            for (int j = 0; j < MAP_HEIGHT; j++) {
-
-            }
+        if (map[newY][newX] == 0) {
+            playerX = newX;
+            playerY = newY;
         }
-
-
     }
 
-    public void playerMove() {
-
-        Rectangle playerBox = new Rectangle((int) PlayerX, (int) PlayerY, TILE_SIZE, TILE_SIZE);
-
-        for (int i = 0; i < MAP_HEIGHT; i++) {
-            for (int j = 0; j < MAP_WIDTH; j++) {
-                    Rectangle wallBox = new Rectangle(j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-                    if (playerBox.intersects(wallBox)) {
-                        if (playerBox.x < wallBox.x) {
-                            PlayerX = wallBox.x - TILE_SIZE;
-                        }
-                        if (playerBox.x + TILE_SIZE > wallBox.x + TILE_SIZE) {
-                            PlayerX = wallBox.x + TILE_SIZE;
-                        }
-                        if (playerBox.y < wallBox.y) {
-                            PlayerY = wallBox.y - TILE_SIZE;
-                        }
-                        if (playerBox.y + TILE_SIZE > wallBox.y + TILE_SIZE) {
-                            PlayerY = wallBox.y + TILE_SIZE;
-                        }
-                    }
-            }
-        }
-
-    }
     public static void main(String[] args) {
         new RpgGame().run();
     }
